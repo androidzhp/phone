a9fd756824829379c195bdb650262bde062b52c7
kabir <jxwazxzhp@126.com>
2016-07-18 20:32:25 +0800

    telephone call.

--- src/com/example/codess/MainActivity.java
+++ src/com/example/codess/MainActivity.java
@@ -1,34 +1,46 @@
 package com.example.codess;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private EditText et_number;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		et_number = (EditText) findViewById(R.id.editText1);
+		Button bt_call = (Button) findViewById(R.id.button1);
+		bt_call.setOnClickListener(new MyClickListener());
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private class MyClickListener implements OnClickListener {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		public void onClick(View v) {
+			String number = et_number.getText().toString();
+			if ("".equals(number)) {
+				Toast.makeText(MainActivity.this, "号码不能为空", Toast.LENGTH_LONG).show();
+				return;
+			}
+
+			Intent intent = new Intent();
+			intent.setAction(Intent.ACTION_CALL);
+			intent.setData(Uri.parse("tel:" + number));
+			startActivity(intent);
 		}
-		return super.onOptionsItemSelected(item);
+
 	}
+
 }
