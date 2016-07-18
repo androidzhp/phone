9a3ca9c682276fd033d5c719f3b393f525101985
kabir <jxwazxzhp@126.com>
2016-07-18 20:53:57 +0800

    .

--- src/com/example/codess/MainActivity.java
+++ src/com/example/codess/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 
 	private EditText et_number;
 
